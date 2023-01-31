39933f4b21597a03d390e300f96d61d35675319e
FerrariFranco <francoferrari22@gmail.com>
2023-01-31 10:11:48 -0300

    MAMAAMAMA

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,5 +2,5 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar() {
-  alert("Hola Mundxdadsasdo");
+  alert("Hola Mundo Mágico");
 }
