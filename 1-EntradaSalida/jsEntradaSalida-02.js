700cc1cc71c55505945d453d1c2462fccc043cc4
FerrariFranco <francoferrari22@gmail.com>
2023-01-31 11:23:57 -0300

    Ejercicio 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,10 @@
-/*
+/*Franco Ferrari Ejercicio 2
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var nombre = prompt("Como es tu nombre?")
+
+	alert(nombre);
 }
 
