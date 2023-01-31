e0ad21e81d189c3751baed99e42e1baad5792b11
FerrariFranco <francoferrari22@gmail.com>
2023-01-31 10:35:50 -0300

    Ejercicio 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,6 +1,12 @@
 /*
+Franco Ferrari
+Div B
+
+
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+
+
 function mostrar() {
-  alert("Hola Mundo Mágico");
+  alert("Esto funciona de maravilla");
 }
