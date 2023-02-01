609f0d13089044a02a93e44e0f17045c25b03270
FerrariFranco <francoferrari22@gmail.com>
2023-02-01 09:21:40 -0300

    asd

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,9 +3,8 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombre = prompt("Cómo es tu nombre?")
+	let nombre = prompt("Cómo es tu nombre?")
 
 	alert(nombre);
 }
 
-alert(nombre);
