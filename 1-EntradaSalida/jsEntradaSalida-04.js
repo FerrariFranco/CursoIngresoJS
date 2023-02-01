281dcadb2235a47ba94e61bce3be29273090a461
FerrariFranco <francoferrari22@gmail.com>
2023-02-01 09:20:31 -0300

    asd

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -2,8 +2,8 @@
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
-function mostrar()
-{
-	alert("ok");
-}
+function mostrar() {
+    let dato = prompt("Ingresa el dato:");
 
+	document.getElementById("txtIdNombre").value = dato;
+  }
