b9522a393f6a9d1adc6e6289ad4611eb0d520814
FerrariFranco <francoferrari22@gmail.com>
2023-02-01 11:33:45 -0300

    as

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,17 @@
-/*
+/*Franco Ferrari Ejercicio 9
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let sueldo;
+	let aumento;
 
-	alert("ok");
+	sueldo = parseInt(document.getElementById("txtIdSueldo").value);
+
+	aumento = sueldo + (sueldo * 0.10);
+
+	document.getElementById("txtIdResultado").value = aumento;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,16 @@
-/*
+/*Franco Ferrari Ejercicio 10
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let importe;
+	let descuento;
+
+	importe = parseInt(document.getElementById("txtIdImporte").value);
+
+	descuento = importe - (importe * 0.25);
+
+	document.getElementById("txtIdResultado").value = descuento;
 }
