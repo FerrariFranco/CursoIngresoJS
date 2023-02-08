678ac82fce632a6efccbd42b3712fb7bc577a5b8
FerrariFranco <francoferrari22@gmail.com>
2023-02-08 11:34:42 -0300

    asd

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,18 +1,43 @@
 /// Franco Ferrari Ejercicio if 1
 function mostrar()
 {
-	//tomo la edad  
-	let edad = parseInt(document.getElementById("txtIdEdad").value);
+//  	let edad = parseInt(document.getElementById("txtIdEdad").value);
 
 
-if(edad > -1){
-	if (edad == 15) {
-		alert("Hola niña bonita");
-	} else {
-		alert("Hola");
-	};
+//  if(edad > -1){
+//  	if (edad == 15) {
+//  		alert("Hola niña bonita");
+//  	} else {
+//  		alert("Hola");
+//  	};
 
-}
-else(alert("No es una edad valida"))
+//  }
+//  else(alert("No es una edad valida"))
+
+
+
+// EJERCICIO 1 BIS
+//  let  tiempoHoras = parseInt(prompt("Cuantas horas tardo?"));
+//  let distanciaRecorrida = parseInt(document.getElementById("txtIdEdad").value);
+//  let kmXHora = distanciaRecorrida / tiempoHoras;
+//  let mensaje;
+
+//  if(tiempoHoras > 0 || distanciaRecorrida > 0 ){
+//  if(kmXHora <= 60){
+//   mensaje ="Demasiado lento"
+//  }else if(kmXHora <= 80){
+//  mensaje ="Lento"
+//  }else if(kmXHora <= 100){
+//  mensaje ="Rapido"
+//  }else if(kmXHora <= 120){
+//  mensaje ="Ahi de la ley"
+//  }else{
+//  	mensaje ="Eso no se hace."
+//  }
+//  } else(alert("No introdujo un numero valido"));
+
+//  alert(mensaje);
+
+
 }
 //FIN DE LA FUNCIÓN
