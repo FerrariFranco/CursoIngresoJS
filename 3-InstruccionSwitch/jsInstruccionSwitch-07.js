7109ddcea5796234ebd875eadea7b105be834f41
FerrariFranco <francoferrari226@gmail.com>
2023-02-09 09:38:07 -0300

    switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,23 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	var destinoIngresado = document.getElementById("txtIdDestino").value;
+	
+	switch(destinoIngresado){	
+	
+    case "Bariloche" :
+	case "Ushuaia" :
+		alert("Sur");
+	break;
+
+	case "Cataratas" :
+		alert("Norte");
+	break;
+	case "Mar del plata" :
+		alert("Este");
+	break;
+	
+	default :
+	alert("Seleccione un destino valido");
+	break;}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,21 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	var destinoIngresado = document.getElementById("txtIdDestino").value;
+	
+	switch(destinoIngresado){	
+	
+    case "Bariloche" :
+	case "Ushuaia" :
+		alert("Frio");
+	break;
+      
+	case "Mar del plata" :
+	case "Cataratas" :
+		alert("Calor");
+	break;
+	
+	default :
+	alert("Seleccione un destino valido");
+	break;}
 
 }//FIN DE LA FUNCIÓN
