c898c132be23585fc7bf04fa9b01f34029caf5ab
FerrariFranco <francoferrari226@gmail.com>
2023-02-09 10:43:30 -0300

    tp6

--- 8-TPs/06-jsAdivinaElNumero2.js
+++ 8-TPs/06-jsAdivinaElNumero2.js
@@ -9,20 +9,45 @@
 Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
-var numeroSecreto; 
-var contadorIntentos;
+var numeroIntroducido; 
+var contadorIntentos = 0;
+var random;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-	
-
+  random = Math.floor(Math.random() * 100) + 1;
+  console.log(random);
+  
 }
 
 function verificar()
 {
+	contadorIntentos = contadorIntentos + 1
+  document.getElementById("txtIdIntentos").value = contadorIntentos
+  numeroIntroducido = parseInt(document.getElementById("txtIdNumero").value)
 	
-	
+
+
+  if(numeroIntroducido < random){
+    alert("falta...")
+ }else if(numeroIntroducido > random){
+  alert("se paso...")
+ }else{
+  if(contadorIntentos <=1){
+   alert("Ganaste! Eres un psiquico!")
+  }else if(contadorIntentos == 2){
+    alert("Excelente percepción, a la segunda!!")
+  }else if(contadorIntentos == 3){
+   alert("Esto es suerte!")
+  }else if(contadorIntentos == 4){
+   alert("Excelente tecnica!")
+  }else if(contadorIntentos == 5){
+	alert("Usted esta en la media!")
+  }else if(contadorIntentos >= 6 && contadorIntentos <= 10){
+     alert("Falta tecnica")
+  }else{
+	alert("Dedicate a otra cosa")
+  }
+}
 
 }
