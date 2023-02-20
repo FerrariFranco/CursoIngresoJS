5be8dc693f94871a2c422af14d2548a87b32831f
FerrariFranco <francoferrari22@gmail.com>
2023-02-20 01:38:06 -0300

    varios

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -1,14 +1,36 @@
 /*
 al presionar el botón mostrar 10 repeticiones 
 con números DESCENDENTES, desde el 10 al 1.*/
-function mostrar()
-{
-	let n
+function mostrar() {
+    // let n
 
-    n = 10
+    // n = 10
 
-    while(n >= 1){
-        alert(n);
-        n =- 1;}
+    // while(n >= 1){
+    //     alert(n);
+    //     n =- 1;}
+    let numeroIngresado;
+    let contador;
+    let contadorDeMayores;
+
+    contador = 0
+
+    contadorDeMayores = 0;
+
+    while (contador < 5) {
+        numeroIngresado = prompt("ingresa un numero");
+        numeroIngresado = parseInt(numeroIngresado);
+        while (isNaN(numeroIngresado)) {
+            numeroIngresado = prompt("error, ingresa un numero valido");
+            numeroIngresado = parseInt(numeroIngresado);
+        }
+        if (numeroIngresado > 9 && numeroIngresado < 21) {
+            contadorDeMayores = contadorDeMayores + 1;
+        }
+
+        contador = contador + 1;
+    }
+
+    alert("la cantidad de numeros que se encuentran entre 10 y 20 (incluidos) es de: " + contadorDeMayores);
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor-05.js
+++ 6-InstruccionFor/jsIteracionesFor-05.js
@@ -1,16 +1,85 @@
 function mostrar() {
 
-	let corte;
-	let cantidadDeCiclos;
+	/*
+		let corte;
+		let cantidadDeCiclos;
+	
+		cantidadDeCiclos = Infinity;
+		for (i = 0; i < cantidadDeCiclos; i++) {
+			corte = prompt("Desea interrumpir el ciclo? coloque 9.")
+			console.log(i);
+			if (corte == "9") {
+				break;
+	
+			}
+		}
+	*/
 
-	cantidadDeCiclos = Infinity;
-	for (i = 0; i < cantidadDeCiclos; i++) {
-		corte = prompt("Desea interrumpir el ciclo? coloque 9.")
-		console.log(i);
-		if (corte == "9") {
-			break;
+
+
+	let notaIngresada;
+	let sexoIngresado;
+	let notaBaja;
+	let banderaMasBaja = true;
+	let sexoBajo;
+	let desaprobados = 0;
+	let notaAcum = 0;
+	let excelentes = 0;
+	let mensajeUno;
+	let mensajeDos = "";
+	let promedio;
+
+
+	for (i = 0; i < 7; i++) {
+
+		notaIngresada = parseInt(prompt("Ingrese la nota"));
+		while (!Number.isInteger(notaIngresada) || notaIngresada > 10 || notaIngresada < 0) {
+			notaIngresada = parseInt(prompt("Ingrese valida la nota"));
+
 
 		}
+		sexoIngresado = prompt("Ingrese sexo (b, m, f)");
+		while (sexoIngresado != "M" && sexoIngresado != "F" && sexoIngresado != "f" && sexoIngresado != "m") {
+			sexoIngresado = prompt("Ingrese sexo valido (b, m, f)");
+
+		}
+		if (sexoIngresado == "M" || sexoIngresado == "m") {
+			sexoIngresado = "Masculino";
+		}
+		if (sexoIngresado == "f" || sexoIngresado == "F") {
+			sexoIngresado = "Femenino";
+		}
+
+		if (banderaMasBaja == true) {
+			notaBaja = notaIngresada;
+			sexoBajo = sexoIngresado;
+			banderaMasBaja = false;
+		}
+
+		else if (notaIngresada < notaBaja) {
+			notaBaja = notaIngresada;
+			sexoBajo = sexoIngresado;
+		}
+
+
+		if (notaIngresada < 4) {
+			desaprobados = desaprobados + 1;
+		}
+
+
+		if ((sexoIngresado == "Masculino") && notaIngresada > 6) {
+			excelentes = excelentes + 1
+			mensajeDos = ", y los alumnos masculinos que sacaron mas de 6 han sido en total " + excelentes;
+		}
+
+
+		notaAcum = notaAcum + notaIngresada;
 	}
 
+
+	promedio = notaAcum / i;
+
+	mensajeUno = "El promedio total de notas es " + promedio + ", la nota de la persona mas baja fue: " + notaBaja + (" y fue de un " + sexoBajo + (", la cantidad total de desaprobados fue de ") + desaprobados + mensajeDos);
+
+	alert(mensajeUno);
 }//FIN DE LA FUNCIÓN
